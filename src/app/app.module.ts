de94b4ee8cc7294040c6da53420278b4592c177b
otus-dn-core <otus@dn-core.ru>
2021-10-10 20:48:30 +0300

    Rout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,12 @@ import { YouTubeSearchComponent } from './you-tube-search/you-tube-search.compon
 import { SearchResultComponent } from './you-tube-search/search-result.component';
 import { SearchBoxComponent } from './you-tube-search/search-box.component';
 import { youTubeSearchInjectables } from './you-tube-search/you-tube-search.injectables';
+
 import {SearchFormComponent} from './you-tube-search/search-form/search-form.component';
 import {SearchTypeComponent} from './you-tube-search/search-type/search-type.component';
+import {SearchElementComponent} from './you-tube-search/search-element/search-element.component';
+import {SearchListComponent} from './you-tube-search/search-list/search-list.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -18,13 +22,16 @@ import {SearchTypeComponent} from './you-tube-search/search-type/search-type.com
     SearchResultComponent,
     SearchBoxComponent,
     SearchFormComponent,
-    SearchTypeComponent
+    SearchTypeComponent,
+    SearchListComponent,
+    SearchElementComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule // <-- right here
+    HttpClientModule, // <-- right here
+    AppRoutingModule
   ],
   providers: [youTubeSearchInjectables],
   bootstrap: [AppComponent]
